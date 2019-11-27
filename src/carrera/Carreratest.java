f44ce1f2df15759bb9d9d05f69ecb2ddee7309e7
AlejandroGurriato <alejandrorova4@gmail.com>
2019-11-27 09:17:16 +0100

    First commit

---
+++ src/carrera/Carreratest.java
@@ -0,0 +1,13 @@
+package carrera;
+
+public class Carreratest {
+	public static void main (string[] args) {
+		Piloto alonso = new Piloto ("Fernando Alonso", "española");
+		coche c = new coche();
+		c.setAnyo(2019);
+	}
+}
+
+alonso.getNombre();
+
+system.out.println ("Hola , me llamo"+ alonso);
